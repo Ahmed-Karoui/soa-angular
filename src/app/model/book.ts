6962876973421a5e8b2a273f01c687b60c99c827
Ahmed Karoui <karouii.ahmed@gmail.com>
2021-03-06 00:54:10 +0100

    some code changes

--- src/app/model/book.ts
+++ src/app/model/book.ts
@@ -1,9 +1,7 @@
 export class Book {
-    constructor(public title: string, public authors: string[], public coverImage: string) {
-        this.previewMode = true;
-    }
-    previewMode: true
-    getAuthorsList() : string {
-        return this.authors.join(", ");
-    }
+ref:String
+nom:String
+genre:String
+authro:String
+first_add:Date
 }
